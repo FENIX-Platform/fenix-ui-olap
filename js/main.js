1e7b75b835cd7e4b735b9b6887653620e677214c
joyeux <joyeux@ESS328.hq.un.fao.org>
2014-12-01 16:52:04 +0100

    Excel export

---
+++ js/main.js
@@ -0,0 +1,46 @@
+requirejs.config({
+
+    baseUrl: '../',
+
+    paths : {
+        jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
+        jqueryui: "lib/jquery-ui-1.9.2.custom.min",
+        i18n: 'lib/jquery.i18n.properties-min',
+        jssc3: "lib/highlight/jssc3",
+        calendar: "lib/grid/calendar/calendar",
+        calendar_utf8: "lib/grid/calendar/calendar-cn-utf8",
+        gt_msg: "lib/grid/gt_msg_en",
+        gt_msg_grid: "lib/grid/gt_grid_all",
+        //fusioncharts: "grid/flashchart/fusioncharts/FusionCharts",        
+        configuration: "js/configuration",
+        pivot: "js/pivot"
+    },
+    shim: {
+        i18n : {
+            deps: ['jquery']
+        },
+        gt_msg: {
+            deps: ['jquery']  
+        },
+        gt_msg_grid: {
+            deps: ['jquery','gt_msg']
+        },
+        pivot: {
+            deps: [
+                'jquery','jqueryui','i18n',
+                'jssc3','calendar','calendar_utf8','gt_msg','gt_msg_grid',
+                'configuration'
+            ]
+        }        
+    }
+});
+
+require(['host','text!dataTest.json','text!dataConfig.json','pivot', 'domReady!'],
+    function( Host, dataTest, dataConfig, pivot ) {
+        
+    dataTest = JSON.parse(dataTest);
+    dataConfig = JSON.parse(dataConfig);
+    
+    $("#testinline").pivotUI(dataTest, dataConfig);
+
+});
