1efe4dcf79310cb16c1da95deb75ae5f0d6e7dae
stefanocudini <stefano.cudini@gmail.com>
2015-05-14 18:22:50 +0200

    added strings i18n

---
+++ nls/pivot.js
@@ -0,0 +1,14 @@
+define({
+	en: true,
+	fr: true,
+	root: {
+	    renderError: "An error occurred rendering the PivotTable results.",
+	    computeError: "An error occurred computing the PivotTable results.",
+	    uiRenderError: "An error occurred rendering the PivotTable UI.",
+	    selectAll: "Select All",
+	    selectNone: "Select None",
+	    tooMany: "(too many to list)",
+	    filterResults: "Filter results",
+	    totals: "Totals", vs: "vs", by: "by"
+	}
+});
