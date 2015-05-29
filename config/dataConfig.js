2311294ecfa4fea37dacb70a8a13eafe635f9204
Kalimaha <guido.barbaglia@gmail.com>
2015-05-29 13:50:57 +0200

    OLAP integration.

---
+++ config/dataConfig.js
@@ -0,0 +1,26 @@
+define({
+	"rows": [
+		"Element",
+		"Area",
+		"Item"
+	],
+	"cols": ["Year"],
+	"vals": ["Value",
+		"Flag"],
+	"hiddenAttributes":[
+		"AreaCode",
+		"ElementCode",
+		"ItemCode",
+		"Unit",
+		"Value",
+		"Flag",
+		"VarOrder1",
+		"VarOrder2",
+		"VarOrder3",
+		"VarOrder4"
+	],
+	"InstanceRenderers":[{label:"Grid",func:"Table"},{label:"Table",func:"Table2"},{label:"HTable",func:"OLAP"}]
+	,"InstanceAggregators":[{label:"SOMME",func:"Sum2"},{label:"Sum",func:"Sum"},{label:"Average",func:"Average"}]
+	,"showAgg":true,
+	"showRender":true
+});		
