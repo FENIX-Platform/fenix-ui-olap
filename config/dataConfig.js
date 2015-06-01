a67fe7636700c383ed60c18c3ce2a2e9bbfe877e
Kalimaha <guido.barbaglia@gmail.com>
2015-06-01 14:59:33 +0200

    OLAP integration.

--- config/dataConfig.js
+++ config/dataConfig.js
@@ -1,26 +1,40 @@
 define({
-	"rows": [
-		"Element",
-		"Area",
-		"Item"
-	],
-	"cols": ["Year"],
-	"vals": ["Value",
-		"Flag"],
-	"hiddenAttributes":[
-		"AreaCode",
-		"ElementCode",
-		"ItemCode",
-		"Unit",
-		"Value",
-		"Flag",
-		"VarOrder1",
-		"VarOrder2",
-		"VarOrder3",
-		"VarOrder4"
-	],
-	"InstanceRenderers":[{label:"Grid",func:"Table"},{label:"Table",func:"Table2"},{label:"HTable",func:"OLAP"}]
-	,"InstanceAggregators":[{label:"SOMME",func:"Sum2"},{label:"Sum",func:"Sum"},{label:"Average",func:"Average"}]
-	,"showAgg":true,
-	"showRender":true
+    "rows": [
+        "Element",
+        "Area",
+        "Item"
+    ],
+    "cols": ["Year"],
+    "vals": [
+        "Value",
+        "Flag"
+    ],
+    "hiddenAttributes": [
+        "AreaCode",
+        "ElementCode",
+        "ItemCode",
+        "Unit",
+        "Value",
+        "Flag",
+        "VarOrder1",
+        "VarOrder2",
+        "VarOrder3",
+        "VarOrder4",
+        "Var1Order",
+        "Var2Order",
+        "Var3Order",
+        "Var4Order",
+        "NoRecords",
+        "RecordOrder"
+    ],
+    "InstanceRenderers": [
+        {label: "Grid", func: "Table"}
+    ],
+    "InstanceAggregators": [
+        {label: "SOMME", func: "Sum2"},
+        {label: "Sum", func: "Sum"},
+        {label: "Average", func: "Average"}
+    ],
+    "showAgg": false,
+    "showRender": false
 });		
