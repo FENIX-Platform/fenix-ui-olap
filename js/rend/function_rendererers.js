0806f5ac9f0795fb6cf17e3e6d00a5aca400d306
deneb1 <deneb1ster@gmail.com>
2015-10-16 11:23:29 +0200

    .

--- js/rend/function_rendererers.js
+++ js/rend/function_rendererers.js
@@ -406,7 +406,7 @@ define([
 											 ret+=addSeparators(r.tree[ligne][coldInd].value()[0]," ",".");
 											if(options.showUnit || options.showFlags){ret+="</td>";}
 											if(options.showUnit){ret+="<td>"+r.tree[ligne][coldInd].value()[1]+"</td>";}
-											if(options.showFlags){ret+="<td>"+r.tree[ligne][coldInd].value()[2]+"</td>";}
+											if(options.showFlags){ret+="<td>("+r.tree[ligne][coldInd].value()[2]+")</td>";}
 											if(options.showUnit || options.showFlags){ret+="</tr></table>";}
 											//temp.push(r.tree[ligne][coldInd].value().join("><"));
 										}
